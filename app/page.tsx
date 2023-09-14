d502a2bd601815534a1d4d402c1dc8d1c06cc952
matmat <matmat@uni-bremen.de>
2023-09-14 20:08:13 +0200

    Fixed duplicated header

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
             <h1 className={`text-4xl mb-8 font-extrabold`}>Das Streben nach Gleichgewicht ist der Schlüssel zum inneren Frieden und persönlichem Wachstum</h1>
             <p>Willkommen im "Hallo Welt" Blog! Entdecken Sie Inspiration und Weisheit für Ihre persönliche Entwicklung. Wir bieten praktische Ratschläge zu Themen wie Zielsetzung, Zeitmanagement und mehr. Gemeinsam werden wir Ihr volles Potenzial entfalten. Ihre Reise beginnt hier!</p>
 
-            <h2 className={`text-2xl mb-6 mt-12`}>Die neusten Artikel</h2>
             <ArticlesGrid articles={displayedArticles} />
         </>
     )
